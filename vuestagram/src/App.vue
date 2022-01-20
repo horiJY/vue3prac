9cba4892d958cc5d4d1b4d1eb5ac5d1a210a8b0b
clouflake-jy <hori_q@naver.com>
2022-01-20 13:03:05 +0900

    테스트 코드 제거

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -1,21 +1,14 @@
 <template>
   <router-view />
   <Spinner :loading="$store.state.LoadingStatus" />
-  <button @click="on">On</button>
 </template>
 
 <script>
 import Spinner from "@/components/Spinner";
-import store from "@/store/store";
 
 export default {
   components: {
     Spinner,
-  },
-  methods: {
-    on() {
-      store.commit("startSpinner");
-    },
   },
 };
 </script>
