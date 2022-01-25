b26b906bf1a840d5eea086e90c89e57c732e4cbe
hori_q@naver.com <hori_q@naver.com>
2022-01-25 10:13:15 +0900

    close#4
    This close #4

--- vuestagram-BE/src/main/java/com/cf/vuestagram/controller/VideoController.java
+++ vuestagram-BE/src/main/java/com/cf/vuestagram/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.cf.vuestagram.controller;
 import java.io.IOException;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.HttpHeaders;
@@ -23,46 +24,44 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Controller
 public class VideoController {
-    private final String baseDir = "file:C:\\Users\\JY\\Desktop\\code\\vuesta-video\\";
+        @Value("${video.base.dir}")
+        private String baseDir;
+        // private final String baseDir =
+        // "file:C:\\Users\\JY\\Desktop\\code\\vuesta-video\\";
 
-    @GetMapping(value = "/video/{name}")
-    public ResponseEntity<ResourceRegion> getVideo(@RequestHeader HttpHeaders headers, @PathVariable String name)
-            throws IOException {
-        // try {
-        log.info("VideoController.getVideo");
-        UrlResource video = new UrlResource(baseDir + name + ".mp4");
-        final long chunkSize = 1024 * 1024;
-        long contentLength = video.contentLength();
-        Optional<HttpRange> optional = headers.getRange().stream().findFirst();
-        ResourceRegion resourceRegion;
-        HttpRange httpRange;
+        @GetMapping(value = "/video/{name}")
+        public ResponseEntity<ResourceRegion> getVideo(@RequestHeader HttpHeaders headers, @PathVariable String name)
+                        throws IOException {
+                // try {
+                log.info("GET /video VideoController " + name);
+                UrlResource video = new UrlResource(baseDir + name + ".mp4");
+                final long chunkSize = 1024 * 1024;
+                long contentLength = video.contentLength();
+                Optional<HttpRange> optional = headers.getRange().stream().findFirst();
+                ResourceRegion resourceRegion;
+                HttpRange httpRange;
 
-        if (optional.isPresent()) {
-            httpRange = optional.get();
-            long start = httpRange.getRangeStart(contentLength);
-            long end = httpRange.getRangeEnd(contentLength);
-            long rangeLength = Long.min(chunkSize, end - start + 1);
-            resourceRegion = new ResourceRegion(video, start, rangeLength);
-            log.info("-resourceRegion(inner if): " + resourceRegion.toString());
-            log.info("-httpRange: " + httpRange);
-        } else {
-            long rangeLength = Long.min(chunkSize, contentLength);
-            resourceRegion = new ResourceRegion(video, 0, rangeLength);
-            log.info("rangeLength:" + rangeLength);
-            log.info("-resourceRegion(inner else): " + resourceRegion.toString());
+                if (optional.isPresent()) {
+                        httpRange = optional.get();
+                        long start = httpRange.getRangeStart(contentLength);
+                        long end = httpRange.getRangeEnd(contentLength);
+                        long rangeLength = Long.min(chunkSize, end - start + 1);
+                        resourceRegion = new ResourceRegion(video, start, rangeLength);
+                } else {
+                        long rangeLength = Long.min(chunkSize, contentLength);
+                        resourceRegion = new ResourceRegion(video, 0, rangeLength);
+                }
+                log.info("return", ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                                .contentType(
+                                                MediaTypeFactory.getMediaType(video)
+                                                                .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                                .body(resourceRegion));
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                                .contentType(
+                                                MediaTypeFactory.getMediaType(video)
+                                                                .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                                .body(resourceRegion);
+
         }
-        log.info("return", ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                .contentType(
-                        MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
-                .body(resourceRegion));
-        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                .contentType(
-                        MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
-                .body(resourceRegion);
-        // } catch (Exception e) {
-        // log.warn(e.getMessage());
-        // }
-
-    }
 
 }
--- vuestagram-BE/src/main/resources/data.sql
+++ vuestagram-BE/src/main/resources/data.sql
@@ -7,9 +7,9 @@ VALUES('Minny','image','https://placeimg.com/640/480/animals',49,'21 Apr 4',fals
 INSERT INTO POST(writer,mediatype,url,likes,date,liked,content,filter)
 VALUES('JY','video','test',77,'22 Jan 10',false,'우리집 개는 화장실 물도 내림','none');
 INSERT INTO POST(writer,mediatype,url,likes,date,liked,content,filter)
-VALUES('salmon_X','video','cutepuppy',2,'22 Jan 10',false,'비맞는 고양이😿','none');
+VALUES('salmon_X','video','rainCat',2,'22 Jan 10',false,'비맞는 고양이😿','none');
 INSERT INTO POST(writer,mediatype,url,likes,date,liked,content,filter)
-VALUES('_Limvely','video','rainCat',7777777,'22 Jan 10',false,'귀여운 강아지','none');
+VALUES('_Limvely','video','cutepuppy',7777777,'22 Jan 10',false,'귀여운 강아지','none');
 
 -- INSERT INTO POST(writer,postimage,likes,date,liked,content,filter)
 -- VALUES('Kim Hyun','https://placeimg.com/640/480/arch',36,'May 15',false,'오늘 무엇을 했냐면요 아무것도 안했어요 ?','perpetua');
