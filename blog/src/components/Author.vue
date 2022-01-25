1b41954396e06cbaddfda62c6a537d47dbaa65ad
hori_q@naver.com <hori_q@naver.com>
2022-01-25 10:19:31 +0900

    blog dir del

--- blog/src/components/Author.vue
+++
@@ -1,7 +0,0 @@
-<template>작가소개</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- blog/src/components/Comment.vue
+++
@@ -1,7 +0,0 @@
-<template>댓글</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
